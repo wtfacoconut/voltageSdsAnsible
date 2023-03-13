5148af6e6d791c4377c6f9fd1630244791a5566f
wtfacoconut <michael.eva.snowden@microfocus.com>
2023-03-13 06:07:04 +0000

    removed vs code server cli instalation

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,6 @@
 
 sudo apt remove -y unattended-upgrades
 sudo apt update
-sudo apt install software-properties-common
+sudo apt install --yes software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible ansible-lint
-
-curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli.tar.gz
-tar -xf vscode_cli.tar.gz
-sudo mv code /usr/bin/.
-rm vscode_cli.tar.gz
+sudo apt install --yes ansible ansible-lint
